17829ce720861367bb358662c9f3bf7c00797a1c
grj <3303272844@qq.com>
2022-07-13 12:05:57 +0800

    完成项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入字体文件图标css
+import '@/assets/fonts/iconfont.css'
+// 引入flexible 动态设置 REM 基准值
+import 'amfe-flexible/index.min.js'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
